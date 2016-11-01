9e29c8949789a38795e6fa79a0aac147118ad7a2
Takashi Masuyama <mamewotoko@gmail.com>
2016-11-01 19:41:01 +0900

    fix build script error

--- jenkins.sh
+++ jenkins.sh
@@ -8,7 +8,6 @@ if [ ! -d target ]; then
    mkdir target
 fi
 curl http://snapshot.bluecove.org/distribution/download/2.1.1-SNAPSHOT/2.1.1-SNAPSHOT.62/bluecove-2.1.1-SNAPSHOT.jar -o lib/bluecove-2.1.1-SNAPSHOT.jar
-mv bluecove-2.1.1-SNAPSHOT.jar lib
 cat > settings.xml << "END_OF_XML";
 <settings>
 <localRepository>/tmp/mavenrepo</localRepository>
